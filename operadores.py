c49ff04c3cc06289752fd5f97e8c459e02186357
ga1ker <2019gadiel.alcazar@cobatlaxcala.edu.mx>
2023-06-09 16:51:53 -0500

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#operadores matematicos
+#+ * - %
+#/división float
+#//división entera
+# **potencia
+print(12+12)
+print(12-12)
+print(12*12)
+print(12%12)
+print(10/3)
+print(10//3)
+print(2**16)
+#operadores para strings
+#+ para concatenar
+#* para replicar
+
+print("___" *4)
+print("Hola "+"Mundo")
+print("Hola "*3)
