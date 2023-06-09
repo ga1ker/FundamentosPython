79574eefd5c6f2b0b76bfd9c01b0edbd39c82f06
ga1ker <2019gadiel.alcazar@cobatlaxcala.edu.mx>
2023-06-09 16:20:26 -0500

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#python es no tipado
+
+name="Gadiel"
+last_name="Alcazar"
+age=19
+height=1.80
+is_student=True
+groups=["3C"]
+
+#función type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
