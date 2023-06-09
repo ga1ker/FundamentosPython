ab014474252ec5b295824bd851bf21e7988b183e
ga1ker <2019gadiel.alcazar@cobatlaxcala.edu.mx>
2023-06-09 16:59:08 -0500

    05 operadores logicos y booleanos

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# operadores booleanos
+# == igualdad
+print(1==1)
+# != distinto
+print(1!=1)
+# > mayor q
+print(1>1)
+# <menor q
+print(1<1)
+#operadores basicos
+#and
+print(True and True)
+print(True and False)
+#or
+print(True or True)
+print(False or False)
+#not
+print(not True)
