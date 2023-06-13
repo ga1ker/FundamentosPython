390e629f5577765678f7acb3292cd4fdf8556541
ga1ker <2019gadiel.alcazar@cobatlaxcala.edu.mx>
2023-06-13 13:01:48 -0500

    07 Strings

---
+++ strings.py
@@ -0,0 +1,26 @@
+my_string="esTe eS Mi StrInG"
+
+#longitud de string
+print(len(my_string))
+#mayusculas
+print(my_string.upper())
+#minusculas
+print(my_string.lower())
+#capitalice
+print(my_string.capitalize())
+#Eliminar espacios inicio fin
+print(my_string.strip())
+
+#indexing and slicing
+my_string="que se te cumplan todas tus metas"
+print(my_string[0])
+print(my_string[1])
+print(my_string[2])
+print(my_string[3])
+print(my_string[6])
+print(my_string[7])
+print(my_string[15])
+print(my_string[16])
+print(my_string[20])
+print(my_string[27])
+print(my_string[28])
